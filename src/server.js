39d1ee1d3b21b04fc9a3fe5ef592356b4a39454e
Daniel Reisel <dreisel@gmail.com>
2018-07-08 11:22:01 +0300

    scripts

--- src/server.js
+++ src/server.js
@@ -1,19 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const webpack = require('webpack');
-const webpackConfig = require('../config/webpack.config.dev');
-const compiler = webpack(webpackConfig);
-
-// webpack hmr
-app.use(
-  require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: webpackConfig.output.publicPath
-  })
-);
-app.use(require("webpack-hot-middleware")(compiler, {
-}));
 app.use(express.static('static'));
 
 // main route
@@ -23,4 +10,4 @@ app.get('*', (req, res) => {
 });
 
 // app start up
-app.listen(3000, () => console.log('App listening on port 3000!'));
+module.exports = app
