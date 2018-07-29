093f4b2aeacbb2a668990da16e5454ba930e6bb4
staskorz <staskorz@gmail.com>
2018-07-30 00:55:32 +0300

    WIP: middleware

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 module.exports = {
-  TRACKING_COOKIE_NAME: 'tracking'
+  DISTANT_FUTURE_DATE: new Date(253402300000000), // Approximately Friday, 31 Dec 9999 23:59:59 GMT
+  TRACKING_COOKIE_NAME: 'tracking',
+  AB_TESTING_COOKIE_NAME: 'ab'
 };
