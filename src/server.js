8398bc33506d81c58f078bedfe5b89545474b164
staskorz <staskorz@gmail.com>
2018-07-30 13:25:29 +0300

    Implemented ab-testing express middleware - naive implementation

--- src/server.js
+++ src/server.js
@@ -4,18 +4,26 @@ const app = express();
 const path = require('path');
 
 const trackingCookieMiddleware = require('./server/express-middleware/tracking-cookie');
+const abTestingMiddleware = require('./server/express-middleware/ab-testing');
+
+const abTestingExperiments = require('../ab-testing-experiments.json').filter(
+  ({ enabled }) => enabled
+);
 
 app.use(cookieParser());
 
 app.use(express.static('static'));
 
 // main route
-app.use('*', trackingCookieMiddleware);
-
-app.get('*', (req, res) => {
-  console.log('got request');
-  res.sendFile(path.resolve(__dirname, '../public/index.html'));
-});
+app.get(
+  '*',
+  trackingCookieMiddleware,
+  abTestingMiddleware(abTestingExperiments),
+  (req, res) => {
+    console.log('got request');
+    res.sendFile(path.resolve(__dirname, '../public/index.html'));
+  }
+);
 
 // app start up
 module.exports = app;
---
+++ src/server/express-middleware/ab-testing/index.js
@@ -0,0 +1,53 @@
+const {
+  DISTANT_FUTURE_DATE,
+  TRACKING_COOKIE_NAME,
+  AB_TESTING_COOKIE_NAME
+} = require('../../../constants');
+
+const tokenToNumber = require('../../ab-testing/cookie/token-to-number');
+
+module.exports = experiments => (req, res, next) => {
+  const trackingToken = req.cookies[TRACKING_COOKIE_NAME];
+
+  const numberDerivedFromToken = tokenToNumber(trackingToken);
+
+  const numberOfExperiments = experiments.length;
+
+  let isCandidate = false;
+
+  for (let i = 0; i < numberOfExperiments; i++) {
+    const experiment = experiments[i];
+
+    if (experiment.conditions) {
+      const { country } = experiment.conditions;
+
+      if (country && req.query.country === country) {
+        if (numberDerivedFromToken < experiment.percentage) {
+          res.cookie(AB_TESTING_COOKIE_NAME, experiment.name, {
+            expires: DISTANT_FUTURE_DATE
+          });
+        }
+
+        isCandidate = true;
+
+        break;
+      }
+    } else {
+      if (numberDerivedFromToken < experiment.percentage) {
+        res.cookie(AB_TESTING_COOKIE_NAME, experiment.name, {
+          expires: DISTANT_FUTURE_DATE
+        });
+      }
+
+      isCandidate = true;
+
+      break;
+    }
+  }
+
+  if (!isCandidate) {
+    res.clearCookie(AB_TESTING_COOKIE_NAME);
+  }
+
+  next();
+};
