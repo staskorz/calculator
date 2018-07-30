ec79a405fb39af3bc258da40bbf87af5557d9223
staskorz <staskorz@gmail.com>
2018-07-30 11:58:23 +0300

    Revert "Fixed a bug with static files serving directory"
    This reverts commit f29f352bc72340feb207376f1af30958d72d1298.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('public'));
+app.use(express.static('static'));
 
 // main route
 app.get('*', (req, res) => {
