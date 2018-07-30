f29f352bc72340feb207376f1af30958d72d1298
staskorz <staskorz@gmail.com>
2018-07-30 11:48:44 +0300

    Fixed a bug with static files serving directory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 // main route
 app.get('*', (req, res) => {
