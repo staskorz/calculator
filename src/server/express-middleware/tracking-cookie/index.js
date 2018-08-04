4c01ef104c0f071917efb8caf1c3e555c53e2cfa
staskorz <staskorz@gmail.com>
2018-08-04 22:57:16 +0300

    Imported distant future cookie expiration date from constants

--- src/server/express-middleware/tracking-cookie/index.js
+++ src/server/express-middleware/tracking-cookie/index.js
@@ -1,14 +1,15 @@
 const generateToken = require('./generate-token');
-const { TRACKING_COOKIE_NAME } = require('../../../constants');
-
-const TRACKING_COOKIE_EXPIRATION_DATE = new Date(253402300000000); // Approximately Friday, 31 Dec 9999 23:59:59 GMT
+const {
+  TRACKING_COOKIE_NAME,
+  DISTANT_FUTURE_DATE
+} = require('../../../constants');
 
 module.exports = (req, res, next) => {
   if (!req.cookies[TRACKING_COOKIE_NAME]) {
     const token = generateToken();
 
     res.cookie(TRACKING_COOKIE_NAME, token, {
-      expires: TRACKING_COOKIE_EXPIRATION_DATE,
+      expires: DISTANT_FUTURE_DATE,
       httpOnly: true
     });
 
